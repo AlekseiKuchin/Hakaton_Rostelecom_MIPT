94d657ba9a098bb7232a16701cd6494059adcd82
Metanick13 <156079974+Metanick13@users.noreply.github.com>
2025-04-10 12:35:37 +0300

    Update __init__.py

--- app1/__init__.py
+++ app1/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flasgger import Swagger
 from .routes import api_blueprint
 
 def create_app():
     app = Flask(__name__)
+    Swagger(app)  # Инициализация Swagger
     app.register_blueprint(api_blueprint, url_prefix="/api")
     return app
