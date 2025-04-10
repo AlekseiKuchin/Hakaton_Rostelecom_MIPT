48e1b53c05b1ff1b0d0a2019337fd0d688dcc890
Metanick13 <156079974+Metanick13@users.noreply.github.com>
2025-04-10 12:34:20 +0300

    Update log_service.py

--- app1/services/log_service.py
+++ app1/services/log_service.py
@@ -1,1 +1,7 @@
+from app.utils.db import clickhouse_query
 
+def get_logs(date=None):
+    query = "SELECT * FROM logs"
+    if date:
+        query += f" WHERE toDate(timestamp) = toDate('{date}')"
+    return clickhouse_query(query)
