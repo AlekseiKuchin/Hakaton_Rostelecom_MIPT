13f1494e4b429563d3c22a87537e9483fb8549ee
Aleksey Nasibulin <nasibulin.aa@phystech.edu>
2025-04-21 12:30:04 +0800

    add db info, add theme switch (no graphs)

--- static/js/logger.js
+++ static/js/logger.js
@@ -4,25 +4,61 @@ var previousPage = null; // variable to store the previous page
 var previousButton = null; // variable to store the previous button
 
 // See https://stackoverflow.com/a/64874674
-function downloadUrl(url){
+function downloadUrl(url) {
     window.open(url, '_self');
+}
+
+// See https://stackoverflow.com/a/39914235
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // For import page (called by event on button)
 async function apacheLogUpload(file, elemProgress, elemMessage) {
     elemProgress.style.display = 'block';
+    elemMessage.style.display = 'none';
     const req = new XMLHttpRequest();
     req.upload.addEventListener("progress", function (evt) {
         var percentage = (evt.loaded / evt.total * 100);
         elemProgress.setAttribute('value', percentage);
     });
-    req.upload.addEventListener('load', function () {
+    req.upload.addEventListener('load', async function (evt) {
         elemProgress.style.display = 'none';
         elemMessage.style.display = 'block';
+        elemProgress.setAttribute('value', 0);
+        elemMessage.textContent = "File uploaded. Waiting DB update..."
+    });
+    req.addEventListener('readystatechange', async function (evt) {
+        // In local files, status is 0 upon success in Mozilla Firefox
+        if (req.readyState === XMLHttpRequest.DONE) {
+            const status = req.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                elemMessage.textContent = "Import complete."
+                getDBstatus("import");
+            } else {
+                elemMessage.textContent = "FAILED TO UPLOAD!"
+            }
+        }
     });
     req.open('POST', '/api/import/apache_log');
     req.setRequestHeader('Content-Type', 'application/octet-stream');
     req.send(file);
+}
+
+function getDBstatus(page_id) {
+    const elem_count = document.querySelector('#' + page_id + ' article p:nth-of-type(1) b');
+    const elem_size = document.querySelector('#' + page_id + ' article p:nth-of-type(2) b');
+    elem_count.setAttribute('aria-busy', true);
+    elem_size.setAttribute('aria-busy', true);
+    fetch('/api/db/db_size').then(async response => {
+        const got_data = await response.json();
+        elem_count.textContent = got_data['count'];
+        elem_size.textContent = got_data['size_human'];
+        elem_count.setAttribute('aria-busy', false);
+        elem_size.setAttribute('aria-busy', false);
+    }).catch(error => {
+        console.error('Error fetching data:', error);
+    });
 }
 
 // On page show
@@ -34,27 +70,30 @@ function pageActionStart() {
             var progress = document.getElementById('graph1_progress');
             fetch('/api/graph_show/graph1').then(async response => {
                 const got_data = await response.json();
+                graph.style.display = 'block';
                 var layout = got_data.layout;
                 layout['autosize'] = true;
                 layout['useResizeHandler'] = true;
                 layout['width'] = "100%";
-                Plotly.newPlot(graph, got_data.data, layout, {responsive: true});
+                Plotly.newPlot(graph, got_data.data, layout, { responsive: true });
                 Plotly.Plots.resize(graph);
                 progress.style.display = 'none';
-                graph.style.display = 'block';
             }).catch(error => {
                 console.error('Error fetching data:', error);
             });
             break;
         case "import":
+            getDBstatus(targetPageId);
             var progress = document.querySelector('#import_form progress');
             var message = document.querySelector('#import_form h3');
             progress.style.display = 'none';
             message.style.display = 'none';
             break;
         case "export_csv":
+            getDBstatus(targetPageId);
             break;
         case "export_parquet":
+            getDBstatus(targetPageId);
             break;
         default:
             alert("Unknown page " + targetPageId);
@@ -121,7 +160,7 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function (event) {
             event.preventDefault()
             const elemFile = document.querySelector('#import_form input[type=file]');
-            const elemProgress= document.querySelector('#import_form progress');
+            const elemProgress = document.querySelector('#import_form progress');
             const elemMessage = document.querySelector('#import_form h3');
             apacheLogUpload(elemFile.files[0], elemProgress, elemMessage);
         });
@@ -132,8 +171,8 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function (event) {
             event.preventDefault()
             const elem = document.querySelector('#export_csv_form input[type=number]');
-            var number = elem.value ? elem.value: 0;
-            downloadUrl("/api/export/csv/"+number)
+            var number = elem.value ? elem.value : 0;
+            downloadUrl("/api/export/csv/" + number)
         });
     }
     // Export to parquet page
@@ -142,8 +181,10 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function (event) {
             event.preventDefault()
             const elem = document.querySelector('#export_parquet_form input[type=number]');
-            var number = elem.value ? elem.value: 0;
-            downloadUrl("/api/export/parquet/"+number)
+            var number = elem.value ? elem.value : 0;
+            downloadUrl("/api/export/parquet/" + number)
         });
     }
+  // Init
+  themeSwitcher.init();
 })
